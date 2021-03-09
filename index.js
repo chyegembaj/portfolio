284797115534982c6de3b9d332c39b9156a29be8
cegemba <cegemba@deseretdigital.com>
2021-03-08 17:49:36 -0700

    updated proxy

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ app.post("/api/form", (req, res) => {
 //   })
 // );
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
