8b8ee39923aaead28a1ce3f08021a2436332df0b
cegemba <cegemba@deseretdigital.com>
2021-03-08 18:13:23 -0700

    updated server index.js file

--- index.js
+++ index.js
@@ -3,11 +3,22 @@ import bodyParser from "body-parser";
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
-
+import path from "path";
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+if (process.env.NODE_ENV === "production") {
+  // Exprees will serve up production assets
+  app.use(express.static("client/build"));
+
+  // Express serve up index.html file if it doesn't recognize route
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 let transport = {
   //host: "smtp.mail.yahoo.com",
@@ -74,16 +85,6 @@ app.post("/api/form", (req, res) => {
 //     },
 //   })
 // );
-if (process.env.NODE_ENV === "production") {
-  // Exprees will serve up production assets
-  app.use(express.static("client/build"));
-
-  // Express serve up index.html file if it doesn't recognize route
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 
 const PORT = process.env.PORT || 5000;
 
